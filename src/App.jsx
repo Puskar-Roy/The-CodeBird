cd222cff076f348a77ab96abbbcfcecb2ff3b207
Puskar-Roy <puskarroy600@gmail.com>
2025-02-16 18:09:05 +0530

    feat : routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
+import { Route, Routes } from "react-router-dom";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
-import Services from "./components/Services";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Routes>
+
+
         <Footer />
       </div>
 
@@ -28,3 +32,40 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+// <BrowserRouter>
+//   <Routes>
+//     <Route
+//       path="/"
+//       element={state.user ? <Home /> : <Navigate to="/login" />}
+//     />
+//     <Route
+//       path="/login"
+//       element={!state.user ? <Login /> : <Navigate to="/" />}
+//     />
+//     <Route
+//       path="/register"
+//       element={!state.user ? <Register /> : <Navigate to="/" />}
+//     />
+//     <Route
+//       path="/forgotPassword"
+//       element={state.user ? <ForgotPassword /> : <Navigate to="/" />}
+//     />
+//     <Route
+//       path="/verifyPassword"
+//       element={state.user ? <VerifyPassword /> : <VerifyPassword />}
+//     />
+//     <Route
+//       path="/uploadPic"
+//       element={state.user ? <UploadPic /> : <Navigate to="/" />}
+//     />
+//     <Route
+//       path="/editor/:roomId/:clientName"
+//       element={<EditorPage />}
+//     />
+//   </Routes>
+// </BrowserRouter>
